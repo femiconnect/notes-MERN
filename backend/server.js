c08d17d2b3569f2ead9c704ecd592f8fdb462174
femi-connect <60244053+femiconnect@users.noreply.github.com>
2022-05-08 03:13:49 +0100

    Illustrate some basic APIs in backend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const notes = require('./data/notes');
 
 const app = express();
 
@@ -8,6 +9,15 @@ dotenv.config();
 
 app.get('/', (req, res) => {
    res.send('API is runnings...');
+});
+
+app.get('/api/notes', (req, res) => {
+   res.json(notes);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+   const note = notes.find((n) => n._id === req.params.id);
+   res.send(note);
 });
 
 const PORT = process.env.PORT || 5000;
